2c3a1f53803f1d41225f3dc2c14994c9dee68782
joycenerd <joycenerd@csie.io>
2020-05-29 11:14:29 +0800

    add lab12

---
+++ exp12/cvheader.h
@@ -0,0 +1,12 @@
+#include <cv.h>
+//#include <cxore.h> 
+#include <highgui.h> 
+#include <stdlib.h> 
+#include <stdio.h> 
+#include <math.h> 
+#include <cv.h> 
+#include <cvaux.h> 
+#include <highgui.h> 
+#include <ml.h> 
+#include <iostream> 
+using namespace std;
